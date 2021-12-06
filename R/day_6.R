8b1c0b59528769f8e7512edbd9c90004b5860830
Michael Jones <michael.jones.gsy@gmail.com>
2021-12-06 22:33:35 +0000

    R day 6

--- R/day_6.R
+++ R/day_6.R
@@ -1,56 +1,51 @@
 library(tidyverse)
 
-
-# ---- part 1 ----
-
-# Given a fish of a certain age, and a period over which to watch it.
-# How many fish will there be at the end including this one
-count_fish <- function(days_left, age, first_cycle = 8, regular_cycle = 6) {
-  if (days_left < age) {
-    1
-  } else {
-    1 + count_decendents(days_left, age, first_cycle, regular_cycle)
-  }
-}
-
-
-count_decendents <- function(days_left, age, first_cycle, regular_cycle) {
-  # Betty is a fish.
-  # The number of Jim's decedents is going to be the number of her children
-  # plus the number of their children.
-  # This will be equal to calling count_fish on each direct child
-  days_left_at_next_cycle <- days_left - age
-  n_direct_children <- ceiling(days_left_at_next_cycle / regular_cycle)
-  childrens_birthdays <- days_left_at_next_cycle - (((seq_len(n_direct_children)-1) * (regular_cycle+1)) + 1)
-  childrens_birthdays <- keep(childrens_birthdays, \(x) x>=0)
-  map_dbl(childrens_birthdays, count_fish,
-          first_cycle, first_cycle, regular_cycle) %>%
-    reduce(`+`, .init = 0)
-  
-}
-
 read_day6 <- function(path) {
   path %>%
     read_file() %>%
     str_split(",", simplify = TRUE) %>%
     as.numeric() %>%
-    tibble(ages = .) %>%
-    group_by(ages) %>%
+    tibble(age = .) %>%
+    group_by(age) %>%
     count()
 }
 
-part1 <- function(path, days_left = 80, first_cycle = 8, regular_cycle = 6) {
+next_generation_old <- function(generation) {
+  generation %>%
+    mutate(age = map(age, step_generation)) %>%
+    unnest_longer(age) %>%
+    group_by(age) %>%
+    summarise(n = sum(n))
+}
+
+next_generation_old <- function(generation) {
+  generation %>%
+    mutate(age = map(age, step_generation)) %>%
+    unnest_longer(age) %>%
+    count(age)
+}
+
+step_generation <- function(x) {
+  x_next = (x - 1)
+  if (x_next == -1) {
+    list(6, 8)
+  } else {
+    list(x_next)
+  }
+}
+
+forward_n_generations <- function(generation, n) {
+  reduce(1:n, function(x, y) {next_generation(x)}, .init = generation)
+}
+
+part1 <- function(path, n) {
   read_day6(path) %>%
-    mutate(fish_for_one = pmap_dbl(list(age = ages),
-                                   count_fish, 
-                                   days_left, first_cycle, regular_cycle)) %>%
-    mutate(total_fish = n * fish_for_one) %>%
-    pull(total_fish) %>%
+    forward_n_generations(n) %>%
+    pull(n) %>%
     sum()
 }
 
-part1("data/6_1.txt", 80)
+part1("data/6_1.txt", n = 80) # 380758
 
-
-
-
+# ---- part 2 ----
+part1("data/6_1.txt", n = 256) # 1710623015163
