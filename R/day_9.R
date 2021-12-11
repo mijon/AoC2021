e05d6d96c219f936441b09c1fa4a301a6b3b0695
Michael Jones <michael.jones.gsy@gmail.com>
2021-12-11 13:38:20 +0000

    R day 9 part 2

--- R/day_9.R
+++ R/day_9.R
@@ -55,5 +55,90 @@ part1 <- function(path) {
   sum(1 + input_mat[lows])
 }
 
-part1("data/9_1.txt")
+part1("data/9_1.txt") # 539
+
+# ---- part 2 ----
+
+m <- read_day9("data/9_1.txt")
+known <- find_lows(m) %>%
+  as_tibble() %>%
+  mutate(group = 1:n())
+
+gen_candidates <- function(r, c, m) {
+  tribble(
+     ~row,  ~col,
+        r, c + 1,
+        r, c - 1,
+    r + 1,     c,
+    r - 1,     c
+  ) %>%
+    filter(row >= 1, row <= nrow(m),
+           col >= 1, col <= ncol(m))
+}
+
+check_values <- function(points, m) {
+  tmp <- points %>% mutate(value = map2_dbl(row, col, ~m[.x, .y])) 
+  # print(tmp)
+  tmp %>%
+    filter(value < 9) %>%
+    select(-value)
+}
+
+move_frontier <- function(state, m) {
+  frontier <- state$frontier
+  known <- state$known
+  
+  frontier %>%
+    mutate(candidates = map2(row, col, gen_candidates, m) %>%
+             map(check_values, m)) %>%
+    select(group, candidates) %>%
+    unnest(candidates) %>%
+    bind_rows() %>%
+    distinct() %>%
+    setdiff(known)
+}
+
+# state is a list(known, frontier)
+step <- function(state, m) {
+  
+  new_frontier <- move_frontier(state, m)
+  # print(nrow(new_frontier))
+  new_state <- list(
+    known = bind_rows(state$known, state$frontier) %>% distinct(),
+    frontier = new_frontier)
+  
+  if (nrow(new_frontier) == 0) {
+    return(new_state)
+  } else {
+    step(new_state, m)
+  }
+}
+
+find_basin <- function(df, m) {
+  step(list(known = df, frontier = df), m)$known
+}
+
+part2 <- function(path) {
+  input <- read_day9(path) 
+  
+  basins <- input %>%
+    find_lows() %>%
+    as_tibble() %>%
+    mutate(group = 1:n(),
+           basin = group) %>%
+    group_by(basin) %>%
+    nest() %>% ungroup() %>%
+    mutate(all_in_basin = map(data, find_basin, m)) %>%
+    select(all_in_basin) %>%
+    unnest(all_in_basin)
+  
+  basins %>%
+    count(group, sort = TRUE) %>%
+    slice_head(n = 3) %>%
+    pull(n) %>%
+    prod()
+}
+
+# This takes a while
+part2("data/9_1.txt") # 73690
 
